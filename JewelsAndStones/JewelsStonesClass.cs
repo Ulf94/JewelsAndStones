38bb0fda2e51507050621678a4a2c3847fa753c6
Ulf94 <79094141+Ulf94@users.noreply.github.com>
2021-12-02 14:06:27 +0100

    Add project files.

---
+++ JewelsAndStones/JewelsStonesClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelsAndStones
+{
+    internal class JewelsStonesClass
+    {
+        public int CountJewelsInStones(string jewels, string stones)
+        {
+            int result = 0;
+
+            foreach(char c in stones)
+            {
+                foreach(char c2 in jewels)
+                {
+                    if(c == c2) result++;
+                }
+            }
+
+            return result;
+        } 
+    }
+}
---
+++ JewelsAndStones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelsAndStones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new JewelsStonesClass();
+            var y = x.CountJewelsInStones("Z", "ZZZZZ");
+            Console.WriteLine(y);
+
+            Console.ReadKey();
+        }
+    }
+}
